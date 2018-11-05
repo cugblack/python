53b991581f285d28377bdd9adce9edc0ea88002c
cugblack <18202794429@163.com>
2018-11-05 16:46:28 +0800

    test ignore

--- redis/app.py
+++ redis/app.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
-import redis
+import redis, time
 import config
 
 pool = redis.ConnectionPool(host = config.HOST, port = config.PORT, decode_responses = True)
 
 r = redis.Redis(connection_pool = config.CONNECTION_POOL)
-#r.set('name','cugblack', ex = 3)
-#print r.get('name')
-#time.sleep(3)
-#print r.get('name')
+r.set('name','cugblack', ex = 3)
+print r.get('name')
+time.sleep(3)
+print r.get('name')
 
 r.hset('hash1', 'k1', 'v1')
 r.hset('hash2', 'k2', 'v2')
