5ce0f8ca5d5f4ab278224c47aef08814d84b9c17
cugblack <18202794429@163.com>
2018-11-06 10:01:33 +0800

    Create config.py

---
+++ auto-scripts/config.py
@@ -0,0 +1,14 @@
+
+#send email
+HOST = "smtp.163.com"
+SENDER_MAIL = "sender@163.com"
+MAIL_AUTH = "163客户端授权码"
+TO = "收件人@qq.com"
+
+#MYSQL
+MYSQL_HOST = ""
+MYSQL_USER = ""
+MYSQL_PASSWD = ""
+MYSQL_DB = ""
+MYSQL_PORT = 3306
+MYSQL_CHARSET = "utf8"
